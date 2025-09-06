072eca74d1eab01adc97aa5d9b9824ff033db9bd
Odpp7 <oddpaz7@gmail.com>
2025-09-06 14:08:10 -0500

    fix: add SearchContent component and update search page

--- app/search/page.js
+++ app/search/page.js
@@ -1,36 +1,6 @@
-"use client";
-import { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import { SearchBar } from "../components/SearchBar";
-import { SideBar } from "../components/SideBar";
-import { FilterTags } from "../components/FilterTags";
-import { VideoList } from "../components/VideoList";
-import SearchVideo from "../services/SearchVideo";
+import SearchContent from "./searchContent";
 
-export default function SearchPage() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [videos, setVideos] = useState([]);
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q");
-
-  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-
-  useEffect(() => {
-    if (query) {
-      SearchVideo(query).then(setVideos);
-    }
-  }, [query]);
-
-  return (
-    <div>
-      <SearchBar onMenuClick={toggleSidebar} />
-      <div className="flex">
-        <SideBar isOpen={isSidebarOpen} />
-        <div className="flex-1">
-          <FilterTags isSidebarOpen={isSidebarOpen} />
-          <VideoList videos={videos} layout="list" />
-        </div>
-      </div>
-    </div>
-  );
+export default function SearchPage({ searchParams }) {
+  const query = searchParams.q || "";
+  return <SearchContent query={query} />;
 }
