6ac1402cf40244bd3a1d928c917def4529db95e9
Odpp7 <oddpaz7@gmail.com>
2025-09-06 15:19:54 -0500

    change on Searchcontent the SearchParams so it can recieve the prop query

--- app/search/searchContent.js
+++ app/search/searchContent.js
@@ -1,15 +1,18 @@
-"use client";
 
+"use client";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { SearchBar } from "../components/SearchBar";
 import { SideBar } from "../components/SideBar";
 import { FilterTags } from "../components/FilterTags";
 import { VideoList } from "../components/VideoList";
 import SearchVideo from "../services/SearchVideo";
 
-export default function SearchContent({ query }) {
+export default function SearchContent() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [videos, setVideos] = useState([]);
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
 
   useEffect(() => {
     if (query) {
