94597c55f153363224f41e962803e3f998e99efb
Odpp7 <oddpaz7@gmail.com>
2025-09-06 14:41:58 -0500

    Minimal change made

--- app/services/SearchVideo.js
+++ app/services/SearchVideo.js
@@ -1,11 +1,14 @@
-const API_KEY = process.env.NEXT_PUBLIC_YT_API_KEY;
-const BASE_URL = "https://www.googleapis.com/youtube/v3";
-
 export default async function SearchVideo(query) {
   try {
+    const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
     const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=${query}&key=${API_KEY}`);
 
-    
+    if (!response.ok) {
+      throw new Error("Error en la petición a YouTube API");
+    }
+
+    const data = await response.json();
+    return data.items;
   } catch (error) {
     console.error("Error en Search.js:", error);
     return [];
